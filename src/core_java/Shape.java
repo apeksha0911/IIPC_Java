4b012caeb65f43d509ae451d80ef27cfcb343ec1
dhruviktank <tankdhruvik29@gmail.com>
2024-05-20 22:35:32 +0530

    first commit

---
+++ src/core_java/Shape.java
@@ -0,0 +1,22 @@
+package core_java;
+
+public class Shape{
+	public static void main(String args[]) {
+		Rectangle rect = new Rectangle();
+		rect.setDim(10, 10);
+		System.out.println(rect.getArea());
+	}
+}
+
+class Rectangle {
+	float length;
+	float breadth;
+	
+	public void setDim(float length, float breadth) {
+		this.length = length;
+		this.breadth = breadth;
+	}
+	public float getArea() {
+		return this.breadth * this.length;
+	}
+}
